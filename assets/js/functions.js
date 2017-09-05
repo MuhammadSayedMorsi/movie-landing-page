7908f97d05694d9eed0b8dbd00991ef548fd7a1e
muhammad sayed <muhammadsaye@yahoo.com>
2017-09-05 04:05:11 +0200

    trigger change

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
 
-  // Get started!
+	$('.menu').click(function (e) {
+		e.stopPropagation();
+		$('#nav_bar').toggleClass('active');
+	});
+
+	$(document).click(function() {
+		$('#nav_bar').removeClass('active');
+	});
 
 });
